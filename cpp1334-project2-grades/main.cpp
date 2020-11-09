51941ecbb2172fa96114f0b543decb2f2fcdd6bb
Pedro Prieto <pedro.prieto003@gmail.com>
2020-11-09 09:32:07 -0500

    Initial commit

---
+++ cpp1334-project2-grades/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp1334-project2-grades
+//
+//  Created by Pedro School on 11/9/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
